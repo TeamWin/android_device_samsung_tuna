f0ea8a76c7493e356061c7d76037632afdf93f14
Ziyan <jaraidaniel@gmail.com>
2015-01-10 01:15:53 +0100

    [CUSTOM] tuna: add hardware key overlays
    Omni: remove not existin config_deviceHardwareWakeKey
    
    Change-Id: I8bba2ddfd5960494b36b699641bbd12d3693e9c3

--- device-omni.mk
+++ device-omni.mk
@@ -1,5 +1,7 @@
 DEVICE_FOLDER := device/samsung/tuna
 
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay-custom
+
 # TWRP Fstab
 PRODUCT_COPY_FILES += \
 	$(DEVICE_FOLDER)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
